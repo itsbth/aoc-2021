daa4a9e82d4ee8445a5018f7b2684ec36d05458e
Bjørn Tore Håvie <bjoern.tore.haavie@pexip.com>
2021-12-01 12:56:53 +0100

    d1: clean up

--- d1/clojure/solve.clj
+++ d1/clojure/solve.clj
@@ -1,24 +1,28 @@
 (require '[clojure.string :as str])
 
+
+(defn parse-int [ns]
+    (Integer/parseInt ns 10))
+
 ; part 1, solved at a repl
-(as-> (slurp "../input") t
-    (str/split t #"\n")
-    (map #(Integer/parseInt % 10) t)
-    (partition 2 1 t)
-    (filter #(reduce < %) t)
-    (count t)
-    (partial printf "Part 1: %d\n"))
+(->> (slurp "../input")
+    (str/split-lines)
+    (map parse-int)
+    (partition 2 1)
+    (filter (partial reduce <))
+    (count)
+    (printf "Part 1: %d\n"))
 
 ; part 2
-(as-> (slurp "../input") t
-    (str/split t #"\n")
-    (map #(Integer/parseInt % 10) t)
-    (partition 3 1 t)
-    (map (partial reduce +) t)
-    (partition 2 1 t)
-    (filter #(reduce < %) t)
-    (count t)
-    (partial printf "Part 2: %d\n"))
+(->> (slurp "../input")
+    (str/split-lines)
+    (map parse-int)
+    (partition 3 1)
+    (map (partial reduce +))
+    (partition 2 1)
+    (filter (partial reduce <))
+    (count)
+    (printf "Part 2: %d\n"))
 
 ;; REMARKS: Not very DRY. Part 2 is a copy of part 1 with two lines added and two lines changed, even reading the file again.
 ;;          Varying parameter order forced me to use as->, making it significantly less clean. Would probably have been worth
